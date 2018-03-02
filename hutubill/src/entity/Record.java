e59550795bc22b0f62e4e1bde4307607f5add1cd
mioskj <155489327@qq.com>
2018-03-02 21:13:49 +0800

    3/2

---
+++ hutubill/src/entity/Record.java
@@ -0,0 +1,51 @@
+package entity;
+
+import java.util.Date;
+
+
+public class Record {
+	public int spend;
+	public int id;
+	public int cid;
+	public String comment;
+	public Date date;
+	
+	public int getId() {
+		return id;
+	}
+	public int getSpend() {
+		return spend;
+	}
+	
+	public int getCid() {
+		return cid;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setSpend(int spend) {
+		this.spend = spend;
+	}
+	
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public void setComment(String comment) {
+		this.comment =comment;
+	}
+	public void setDate(Date date) {
+		
+		this.date = date;
+	}
+
+}
